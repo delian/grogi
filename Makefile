1600eef47e624da6947d0cbc362ee7da787ab9c7
Delian Delchev <delian.delchev@gmail.com>
2017-07-14 15:46:14 +0200

    Small updates
    Signed-off-by: Delian Delchev <delian.delchev@gmail.com>

---
+++ Makefile
@@ -0,0 +1,11 @@
+all: build
+
+godeps:
+	go get github.com/gorilla/mux
+	go get github.com/BurntSushi/toml
+
+build: godeps
+	go build
+
+clean:
+	rm -rf grogi
--- httpserver/http.go
+++ httpserver/http.go
@@ -2,22 +2,20 @@ package httpserver
 
 import (
 	"../config"
+	"github.com/gorilla/mux"
 	"log"
 	"net/http"
 	"strconv"
 	"time"
 )
 
-func myHandler(w http.ResponseWriter, r *http.Request) {
-
-}
-
 func Run() {
+	router := mux.NewRouter()
 	port := config.Config.Httpserver.Port
 	log.Println("HTTP Proxy started. Management is listening on", "0.0.0.0:"+strconv.Itoa(port))
 	s := &http.Server{
-		Addr: "0.0.0.0:" + strconv.Itoa(port),
-		//Handler:        myHandler,
+		Addr:           "0.0.0.0:" + strconv.Itoa(port),
+		Handler:        router,
 		ReadTimeout:    10 * time.Second,
 		WriteTimeout:   10 * time.Second,
 		MaxHeaderBytes: 1 << 20,
