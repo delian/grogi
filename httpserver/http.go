c6f40c9d429ce312c4b888861accf2bdcc200954
Delian Delchev <delian.delchev@gmail.com>
2017-07-14 15:03:08 +0200

    Small fix
    Signed-off-by: Delian Delchev <delian.delchev@gmail.com>

--- httpserver/http.go
+++ httpserver/http.go
@@ -1,9 +1,26 @@
 package httpserver
 
-import "fmt"
-import "../config"
+import (
+	"../config"
+	"log"
+	"net/http"
+	"strconv"
+	"time"
+)
 
-func Test() {
+func myHandler(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func Run() {
 	port := config.Config.Httpserver.Port
-	fmt.Println("Test Port", port)
+	log.Println("HTTP Proxy started. Management is listening on", "0.0.0.0:"+strconv.Itoa(port))
+	s := &http.Server{
+		Addr: "0.0.0.0:" + strconv.Itoa(port),
+		//Handler:        myHandler,
+		ReadTimeout:    10 * time.Second,
+		WriteTimeout:   10 * time.Second,
+		MaxHeaderBytes: 1 << 20,
+	}
+	log.Fatal(s.ListenAndServe())
 }
--- main.go
+++ main.go
@@ -7,5 +7,5 @@ import (
 
 func main() {
 	config.Read()
-	httpserver.Test()
+	httpserver.Run()
 }
